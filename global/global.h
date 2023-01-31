a0fba393d0d683e2848751d9b917c42c96b4d957
pitkajuh <105305911+pitkajuh@users.noreply.github.com>
2023-01-31 15:43:10 +0000

    Add files via upload

---
+++ global/global.h
@@ -0,0 +1,30 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#include <string>
+
+using namespace std;
+
+extern const string multiply;
+extern const string divide;
+extern const string add;
+extern const string subtract;
+extern const string power;
+extern const string open_parenthesis;
+extern const string close_parenthesis;
+extern const string comment;
+extern const string curly_bracket_o;
+extern const string curly_bracket_c;
+extern const string origin_element;
+extern const string void_element;
+extern const string not_found;
+extern const string parm_delim;
+extern const string delimiter;
+extern const string equal_sign;
