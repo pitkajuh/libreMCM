5c8012ba16c5fa049c6d750759f3a81909a974d9
pitkajuh <105305911+pitkajuh@users.noreply.github.com>
2023-01-31 15:42:35 +0000

    Create global.cpp

---
+++ global/global.cpp
@@ -0,0 +1,30 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#include <string>
+
+using namespace std;
+
+extern const string multiply="*";
+extern const string divide="/";
+extern const string add="+";
+extern const string subtract="-";
+extern const string power="^";
+extern const string open_parenthesis="(";
+extern const string close_parenthesis=")";
+extern const string comment="//";
+extern const string curly_bracket_o="{";
+extern const string curly_bracket_c="}";
+extern const string origin_element="origin";
+extern const string void_element="void";
+extern const string not_found="Not found";
+extern const string parm_delim="@";
+extern const string delimiter=";";
+extern const string equal_sign="=";
