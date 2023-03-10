a630f0c0ac544fae7edbb07d52f054d9d23593b8
pitkajuh <105305911+pitkajuh@users.noreply.github.com>
2023-03-10 20:14:22 +0000

    Create global.h

---
+++ global/global.h
@@ -0,0 +1,35 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <string>
+
+using std::string;
+
+extern const string multiply;
+extern const string divide;
+extern const string add;
+extern const string subtract;
+extern const string power;
+extern const string open_parenthesis;
+extern const string close_parenthesis;
+extern const string comment;
+extern const string curly_bracket_o;
+extern const string curly_bracket_c;
+extern const string not_found;
+extern const string parm_delim;
+extern const string delimiter;
+extern const string equal_sign;
+extern const string origin_element;
+extern const string void_element;
+
+#endif
