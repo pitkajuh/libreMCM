6e1295af5a7c03f4a677ff6513b4c07517850c46
pitkajuh <105305911+pitkajuh@users.noreply.github.com>
2023-03-16 14:49:43 +0000

    Add files via upload

---
+++ wdata/values_to_graph.h
@@ -0,0 +1,21 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#ifndef VALUESTOGRAPH_H
+#define VALUESTOGRAPH_H
+
+#include "../class/AllInitialValues.h"
+
+extern vector<string> all_compartments;
+extern vector<string> all_parameters;
+extern AllInitialValues initial_values_all;
+void add_all_values();
+
+#endif
---
+++ wdata/write_to_file.h
@@ -0,0 +1,18 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#ifndef WRITETOFILE_H
+#define WRITETOFILE_H
+
+using std::string;
+
+void write_to_file(const string directory);
+
+#endif
