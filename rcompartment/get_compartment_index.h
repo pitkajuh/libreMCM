67f4dd7ea241cb01cc5d583bd20028b2c6752e88
pitkajuh <105305911+pitkajuh@users.noreply.github.com>
2023-03-16 14:48:56 +0000

    Add files via upload

---
+++ rcompartment/get_compartment_index.h
@@ -0,0 +1,20 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#ifndef GETCOMPARTMENTINDEX_H
+#define GETCOMPARTMENTINDEX_H
+
+#include <string>
+
+using std::string;
+
+int get_compartment_index(string compartment_name);
+
+#endif
---
+++ rcompartment/get_parameter_value.h
@@ -0,0 +1,18 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#ifndef GETPARAMETERVALUE_H
+#define GETPARAMETERVALUE_H
+
+using std::string;
+
+string get_compartment_parameter_value(const string parameter_wanted);
+
+#endif
