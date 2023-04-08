0174270a4c2a1062ab0463b8bb12f6546daac9ec
pitkajuh <105305911+pitkajuh@users.noreply.github.com>
2023-04-08 11:23:48 +0000

    Create s2d.h

---
+++ util/s2d.h
@@ -0,0 +1,20 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#ifndef STOD_H
+#define STOD_H
+
+#include <string>
+
+using std::string;
+
+double s2d(const string value);
+
+#endif
