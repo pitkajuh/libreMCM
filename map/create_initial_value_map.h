6f6621a32c35bb022ae1c82bbeb6ff14852a4899
pitkajuh <105305911+pitkajuh@users.noreply.github.com>
2023-04-08 11:25:00 +0000

    Create create_initial_value_map.h

---
+++ map/create_initial_value_map.h
@@ -0,0 +1,16 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+|                               +===========+                                |
+|                               | l         |                                |
+|                               |   i       | M ulti                         |
+|                               |     b     | C ompartment                   |
+|                               |       r   | M odelling                     |
+|                               |         e |                                |
+|                               +===========+                                |
+\*---------------------------------------------------------------------------*/
+
+#ifndef CREATEINITIALVALUEMAP_H
+#define CREATEINITIALVALUEMAP_H
+
+void create_initial_value_map();
+
+#endif
