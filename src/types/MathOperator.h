45d0260ee326785bb9cab827bdd3dbbd77ddebcf
pitkajuh <github_pj@protonmail.com>
2024-06-19 22:13:50 +0300

    Edit

--- src/types/MathOperator.h
+++ src/types/MathOperator.h
@@ -8,54 +8,49 @@
 |                               +===========+                                |
 \*---------------------------------------------------------------------------*/
 
-#ifndef MATHOPERATION_H
-#define MATHOPERATION_H
+#ifndef MATHOPERATOR_H
+#define MATHOPERATOR_H
 
 #include "Value.h"
-#include "MathOperator.h"
 // #include "../global/matrix.h"
 #include "../global/mathconst.h"
 // #include "../util/is_in_vector.h"
 
-class MathOperation
+class MathOperator
 {
 public:
-  string id;
-  Value* v1;
-  Value* v2;
-  MathOperator* math_op;
-  double result;
+  virtual double Calculate(const double v1, const double v2)=0;
+  ~MathOperator(){}
+};
 
-  // void SetValue1(const string x)
-  // {
-  //   bool variable=is_in_vector(Matrix.diagonal2, x);
-  //   if(variable) v1=new Variable;
-  //   else v1=new Constant(x);
-  // }
+class Add:public MathOperator
+{
+public:
+  double Calculate(const double v1, const double v2) return v1+v2;
+};
 
-  // void SetValue2(const string x)
-  // {
-  //   bool variable=is_in_vector(Matrix.diagonal2, x);
-  //   if(variable) v2=new Variable;
-  //   else v2=new Constant(x);
-  // }
+class Sub:public MathOperator
+{
+public:
+  double Calculate(const double v1, const double v2) return v1-v2;
+};
 
-  void SetMathOp(const string m)
-  {
-    if(m==ADD) math_op=new Add;
-    else if(m==SUBTRACT) math_op=new Sub;
-    else if(m==MULTIPLY) math_op=new Mul;
-    else if(m==DIVIDE) math_op=new Div;
-    else if(m==EXP) math_op=new Exp;
-  }
+class Mul:public MathOperator
+{
+public:
+  double Calculate(const double v1, const double v2) return v1*v2;
+};
 
-  ~MathOperation()
-  {
-    delete math_op;
-    delete v1;
-    delete v2;
-  }
+class Div:public MathOperator
+{
+public:
+  double Calculate(const double v1, const double v2) return v1/v2;
+};
 
+class Exp:public MathOperator
+{
+public:
+  double Calculate(const double v1, const double v2) return v1^v2;
 };
 
 #endif
