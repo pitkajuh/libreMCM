8f128bbe627680d28d9e2885544b29f050ea4fe4
pitkajuh <github_pj@protonmail.com>
2024-07-05 22:55:39 +0300

    Edit

--- src/util/IsIn.h
+++ src/util/IsIn.h
@@ -15,14 +15,16 @@
 #include <vector>
 #include <unordered_map>
 #include "../types/MathOperation.h"
+#include "../inc/namespace.h"
 
+using namespace libremcm;
 using std::string;
 using std::vector;
 using std::unordered_map;
 using MathOperations=unordered_map<string, OpTmp>;
 
 bool IsNumerical(const string &s);
-bool IsIn(const string &s, const unordered_map<string, string> &f);
+bool IsIn(const string &s, const smap &f);
 bool IsIn(const string &s, const MathOperations &f);
 bool IsIn(const string &s, const vector<string> &f);
 
