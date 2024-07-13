f6cfb5be7e2dea1068f906d0762785ac3b8c1b81
pitkajuh <github_pj@protonmail.com>
2024-07-13 13:46:25 +0300

    Edit

--- src/eqs/CreateEquationTemplates.cpp
+++ src/eqs/CreateEquationTemplates.cpp
@@ -11,15 +11,15 @@
 #include "../util/IsIn.h"
 #include "../types/MathOperation.h"
 #include "../types/Csv.h"
+#include "../inc/namespace.h"
 #include <iostream>
 #include <stdexcept>
-#include "../inc/namespace.h"
 
 using namespace libremcm;
 using std::cout;
 using MathOperations=Map<string, OpTmp>;
 
-Value *ValueCheck(const string &s, const vector<string> &diagonal, SMap &constants_map, const MathOperations &v)
+Value *ValueCheck(const string &s, const vector<string> &diagonal, SMap &constants_map, const MathOperations &v,  vector<MathOperation*> &ops)
 {
   const bool is_variable=IsIn(s, diagonal);
   const bool is_constant=IsIn(s, constants_map);
@@ -47,6 +47,9 @@ Value *ValueCheck(const string &s, const vector<string> &diagonal, SMap &constan
     }
   else if(is_math)
     {
+      const unsigned int i=std::stoi(s.substr(1, s.size()));
+      cout<<"is math "<<i<<" "<<ops.size()<<" "<<'\n';
+      MathOperation *op=ops[i];
       Value *v=new MathOperationValue;
       return v;
     }
@@ -59,17 +62,20 @@ MathOperation *GetValue(MathOperations v, const vector<string> &diagonal, SMap &
   Value *v1;
   Value *v2;
   MathOperation *op=new MathOperation;
+  vector<MathOperation*> ops;
 
-  for(unsigned i=0; i<v.size(); i++)
+  for(unsigned int i=0; i<v.size(); i++)
     {
       s="@"+std::to_string(i);
-      v1=ValueCheck(v[s].value1, diagonal, constants_map, v);
-      v2=ValueCheck(v[s].value2, diagonal, constants_map, v);
+      v1=ValueCheck(v[s].value1, diagonal, constants_map, v, ops);
+      v2=ValueCheck(v[s].value2, diagonal, constants_map, v, ops);
       cout<<v.size()<<" "<<s<<" "<<v[s].value1<<v[s].math_operator<<v[s].value2<<'\n';
+      cout<<" "<<'\n';
       op->v1=v1;
       op->SetMathOp(v[s].math_operator);
       op->v2=v2;
-      op->LinkMathOp();
+      // op->LinkMathOp();
+      ops.emplace_back(op);
       delete v1;
       delete v2;
     }
