f724e18f5673dff08f8323cc6738ba8a060255ce
pitkajuh <github_pj@protonmail.com>
2024-10-14 21:05:17 +0300

    Edit

--- src/eqs/ToVector.cpp
+++ src/eqs/ToVector.cpp
@@ -32,6 +32,20 @@ void print_vector3(const vector<string> &vec)
     }
   cout<<empty<<'\n';
 }
+
+const int16_t FindIndexSub(const string &s, int16_t i)
+{
+  const int16_t found=s.find(OPERATORS2[i]);
+  const int16_t size=OPERATORS2.size();
+
+  if (found==-1 and i<size)
+    {
+      i++;
+      return FindIndexSub(s, i);
+    }
+  return i;
+}
+
 const int16_t FindIndex(const string &s)
 {
   const uint8_t size=OPERATORS2.size();
@@ -73,7 +87,7 @@ void StringConvert(string &v, vector<string> &aa)
 vector<string> ToVector(string v)
 {
   const uint16_t size=v.size();
-  assert(size!=2 and size>0 && "The equation {v} is too short.");
+  assert(size!=2 and size>0 && "The equation is too short.");
   vector<string> r;
   StringConvert(v, r);
   assert(r.size()<=UINT8_MAX && "Equation size limit exceeded!");
