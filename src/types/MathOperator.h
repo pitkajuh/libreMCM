e6295cbb1bde47bc4e0393cea0e87ff916ad3700
pitkajuh <github_pj@protonmail.com>
2024-11-20 21:45:05 +0200

    Edit

--- src/types/MathOperator.h
+++ src/types/MathOperator.h
@@ -16,6 +16,12 @@ class MathOperator
 public:
   MathOperator(){}
   MathOperator(const MathOperator &m){}
+  MathOperator &operator=(MathOperator &m)
+  {
+    if(this==&m) return *this;
+
+    return *this;
+  }
   virtual MathOperator *clone()=0;
   virtual double Calculate(const double v1, const double v2)=0;
   virtual MathOperator *New()=0;
@@ -30,6 +36,12 @@ public:
   Add(){}
   Add(const MathOperator &m){}
   Add *clone() override {return new Add(*this);}
+  Add &operator=(MathOperator &m)
+  {
+    if(this==&m) return *this;
+
+    return *this;
+  }
 };
 
 class Sub:public MathOperator
@@ -40,6 +52,12 @@ public:
   Sub(){}
   Sub(const MathOperator &m){}
   Sub *clone() override {return new Sub(*this);}
+  Sub &operator=(MathOperator &m)
+  {
+    if(this==&m) return *this;
+
+    return *this;
+  }
 };
 
 class Mul:public MathOperator
@@ -50,6 +68,12 @@ public:
   Mul(){}
   Mul(const MathOperator &m){}
   Mul *clone() override {return new Mul(*this);}
+  Mul &operator=(MathOperator &m)
+  {
+    if(this==&m) return *this;
+
+    return *this;
+  }
 };
 
 class Div:public MathOperator
@@ -60,6 +84,12 @@ public:
   Div(){}
   Div(const MathOperator &m){}
   Div *clone() override {return new Div(*this);}
+  Div &operator=(MathOperator &m)
+  {
+    if(this==&m) return *this;
+
+    return *this;
+  }
 };
 
 class Exp:public MathOperator
@@ -70,6 +100,12 @@ public:
   Exp(){}
   Exp(const MathOperator &m){}
   Exp *clone() override {return new Exp(*this);}
+  Exp &operator=(MathOperator &m)
+  {
+    if(this==&m) return *this;
+
+    return *this;
+  }
 };
 
 #endif
